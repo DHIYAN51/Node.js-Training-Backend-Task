1183403854cbe70661b0da4877619bee4ac17ec7
DHIYAN51 <dhiyaneshkannan.r@indusnet.co.in>
2023-02-03 23:52:53 +0530

    cors

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 require('dotenv').config();
-// app.use(cors(
-//     {
-//         origin: "*"
-//     }
-// ))
+app.use(cors(
+    {
+        origin: "*"
+    }
+))
 
 
 // app.get('/',(req,res)=>{
