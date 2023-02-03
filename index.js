55a602799e16e9caf03b65357cb48e7660341ad5
DHIYAN51 <dhiyaneshkannan.r@indusnet.co.in>
2023-02-03 20:35:17 +0530

    second init

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8000
+const express = require('express');
+const cors = require('cors');
+const mongoose  = require('mongoose');
+
+require('dotenv').config();
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,err=>{
+    if(err) throw err;
+})
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("Mongoose database connection established successfully")
+})
+
+const port = 8000
 app.listen(port,()=>{
     console.log(`server is running in port ${port}`);
 })
+// app.listen(8001)
